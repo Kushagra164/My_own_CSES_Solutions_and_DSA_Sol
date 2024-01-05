a6e46e129c7a1d8ad11d52c8f7dd4b125e566c1b
Kushagra164 <20cs02004@iitbbs.ac.in>
2024-01-05 12:28:05 +0530

    One Centre added

---
+++ Computational Geometry/OneCentre.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define ll long long
+#define pb push_back
+#define all(a) a.begin(),a.end()
+#define MOD 1000000007
+
+using namespace std;
+
+//Code By Kush
+
+/*
+Q) Given a set of points find a point and radius such that the circle made by  
+*/
+
+class Point{
+    int x,y;
+    Point(int i,int j){
+        this->x = i;
+        this->y = j;
+    }
+};
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    rep(i,0,n) {
+        cin >> arr[i];
+    }
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+    return 0;
+}
